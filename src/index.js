2703f741a4fdbd8efa6c0c22e7458bbfcda52d9a
David Brockman Smoliansky <david@springworks.se>
2015-09-07 19:45:50 +0200

    Fix linter issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { zipObject } from './internal/zip-object';
 
 export function allObject(object) {
   const keys = Object.keys(object);
-  const values = keys.map(key => object[key]);
-  return Promise.all(values).then(values => zipObject(keys, values));
+  return Promise.all(keys.map(key => object[key])).then(values => zipObject(keys, values));
 }
-
